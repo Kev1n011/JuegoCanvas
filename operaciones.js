f93fb1680d17cf0d106acdc1d606351b9074c0dc
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-09-12 21:25:45 -0700

    Sprites, sonidos, pausa
    Añadido: Sprites del jugador, capacidad para disparar por parte del jugador, menú de pausa, y sonidos a la hora de disparar

--- operaciones.js
+++ operaciones.js
@@ -12,10 +12,38 @@ var obstaculos = [];
 var pausa = false;
 var score = 0;
 
+var velocidad_bala = 2;
+var balas = [];
+
+//SPRITES DEL JUGADOR
+const player_sprite = new Image();
+player_sprite.src = "./imagenes/player/player.png"
 
 
 
-class Rectangulo {
+player_sprite.onload = function () {
+    ctx.drawImage(player_sprite, this.x, this.y, 200, 100);
+};
+
+const imagen_bala = new Image();
+imagen_bala.src = "./imagenes/balas/bala.png"
+imagen_bala.onload = function () {
+    ctx.drawImage(imagen_bala, this.x, this.y, 800, 100);
+};
+
+//SONIDOS
+function sonido_disparo() {
+    var audio = new Audio('./sonidos/disparo.wav');
+    audio.volume = 0.1;
+    audio.play();
+
+}
+
+
+
+
+
+class Player {
     constructor(x, y, ancho, alto, color) {
         this.x = x;
         this.y = y;
@@ -25,14 +53,29 @@ class Rectangulo {
     }
 
     dibujar() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.ancho, this.alto);
+        ctx.drawImage(player_sprite, this.x, this.y, 80, 80);
+    }
+
+    disparar() {
+        let posicion_x = 0;
+        let posicion_y = 0;
+
+        //Dependiendo del lado del personaje, acomodar la posición de donde saldrá la bolita
+        posicion_x = 35;
+        posicion_y = -35;
+        balas.push({
+            x: player.x + posicion_x,
+            y: player.y + posicion_y,
+            velocidad: velocidad_bala,
+            direccion: direccion
+        });
+        sonido_disparo();
     }
 
 }
 
 
-const player = new Rectangulo(100, 500, 100, 100, "red");
+const player = new Player(100, 500, 100, 100, "red");
 
 
 
@@ -53,6 +96,7 @@ document.addEventListener("keydown", function (e) {
         }
 
         if (teclapresionada['KeyC']) {
+            player.disparar();
         }
     }
 
@@ -61,18 +105,11 @@ document.addEventListener("keydown", function (e) {
     }
 });
 
+
 document.addEventListener("keyup", function (e) {
     delete teclapresionada[e.code];
 });
-document.addEventListener("keypress", function (e) {
-    var tecla_presionada = e.keyCode;
-    console.log("tecla: " + tecla_presionada)
 
-    if (tecla_presionada == 32) {
-        pausa = !pausa;
-    }
-    //a = 97 s = 115 d = 100 w = 119
-});
 
 function update() {
     if (!pausa) {
@@ -92,10 +129,17 @@ function update() {
                 }
         }
 
+        // Actualiza y dibuja balas
+
+        balas.forEach((bala, index) => {
+            bala.y -= bala.velocidad;
+            ctx.drawImage(imagen_bala, bala.x, bala.y, 10, 30);
+        });
+
+
 
 
     }
-
 
 
 }
@@ -109,7 +153,7 @@ function paint() {
 
 
 
-   
+
     player.dibujar();
     ctx.font = "30px Georgia";
     ctx.fillStyle = "gray";
