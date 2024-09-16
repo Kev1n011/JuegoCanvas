78c77cba7cf2a30bee1089767bb919639f098b94
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2024-09-15 20:15:26 -0700

    Menú de inicio, Menú de game over, vidas del jugador, música de fondo, puntuación
    - Se han añadido los menús de inicio y de game over, el usuario podrá iniciar o reiniciar el juego una si interactúa con estos.
    -El jugador ahora pierde vidas si este recibe un impacto de bala por parte del enemigo, si su vida llega a 0, se muestra el menú de game over y finaliza el juego.
    -Se añadió múscia de fondo, esta solo inicia cuando el jugador está en partida.
    - Los enemigos ahora dan puntos si son eliminados, la puntuación se actualiza y se muestra en un apartado de la pantalla, también se muestra la puntuación final obtenida al terminar el juego.
    
    CAMBIOS REALIZADOS.
    - Se ha reemplazado la animación del enemigo que se reproducía a la hora de destruise. Ahora, en lugar de una animación, se muestra un texto indicando la cantidad de puntos que ha dado ese enemigo, este texto desaparece pasado cierto tiempo.

--- operaciones.js
+++ operaciones.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+let juego_terminado = false;
 
 var direccion = "";
 var velocidad = 1.2;
@@ -8,12 +9,16 @@ var obstaculos = [];
 
 var pausa = false;
 var score = 0;
+const puntuaciones = [];
 
 var velocidad_bala = 2;
 var balas = [];
 
 var balas_enemigo = [];
-var velocidad_bala_enemigo = 2;
+var velocidad_bala_enemigo = 1;
+
+let bandera_destruido = false;
+let mostrar_menu_inicio = true;
 
 //BACKGROUND DEL JUEGO
 const fondo_juego = new Image();
@@ -22,6 +27,40 @@ fondo_juego.src = "./imagenes/Niveles_Fondos/fondo_3.jpg"
 fondo_juego.onload = function () {
     ctx.drawImage(fondo_juego, 0, 0, canvas.width, canvas.height);
 };
+
+//MENÚ DE INICIO
+const img_menu_inicio = new Image();
+img_menu_inicio.src = "./imagenes/Niveles_Fondos/menu_inicio.png"
+
+img_menu_inicio.onload = function () {
+    ctx.drawImage(img_menu_inicio, 0, 0, canvas.width, canvas.height);
+};
+
+function menu_inicio() {
+    ctx.drawImage(img_menu_inicio, 0, 0, canvas.width, canvas.height);
+
+    ctx.textAlign = "left"; 
+    ctx.fillStyle = "white";
+    ctx.font = "60px Unlock";
+    ctx.fillText("Pulsa Enter para comenzar", canvas.width - 860, 500);
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = .4;
+    ctx.strokeText("Pulsa Enter para comenzar", canvas.width - 860, 500);
+}
+
+function game_over() {
+    ctx.fillStyle = "rgba(34,37, 95, 0.8)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "rgba(217,217,217)";
+    ctx.font = "60px Unlock";
+    ctx.textAlign = "center";  
+    ctx.fillText("Game Over", canvas.width / 2, (canvas.height / 2) - 200);
+
+    ctx.font = "60px Noto Sans";
+    ctx.fillText("Puntuación: " + score, canvas.width / 2, (canvas.height / 2) - 100);
+}
 //SPRITES DEL JUGADOR
 const player_sprite = new Image();
 player_sprite.src = "./imagenes/player/player.png"
@@ -59,20 +98,29 @@ function sonido_disparo() {
     audio.volume = 0.1;
     audio.play();
 
+
 }
-function sonido_enemigo_destruido(){
+function sonido_enemigo_destruido() {
     var audio = new Audio('./sonidos/enemigo_destruido.wav');
-    audio.volume = 0.1;
-    audio.autoplay = true;
+    audio.volume = 0.060;
     audio.play()
 }
+function sonido_player_dano() {
+    var audio = new Audio('./sonidos/player_dano.wav');
+    audio.volume = 0.360;
+    audio.play()
+}
+const musica_fondo = new Audio('./Sonidos/Necrofantasia-8-Bit-Remix.wav');
+musica_fondo.volume = 0.3;
+
 
 class Player {
-    constructor(x, y, ancho, alto) {
+    constructor(x, y, ancho, alto, puntos_hp) {
         this.x = x;
         this.y = y;
         this.ancho = ancho;
         this.alto = alto;
+        this.puntos_hp = puntos_hp;
     }
 
     dibujar() {
@@ -95,48 +143,38 @@ class Player {
         sonido_disparo();
     }
 
+    manejarColision(bala) {
+        return (player.x < bala.x + 50 &&
+            player.x + player.ancho > bala.x &&
+            player.y < bala.y + 50 &&
+            player.y + player.alto > bala.y) ||
+            (player.x > bala.x - 50 &&
+                player.x + player.ancho < bala.x &&
+                player.y > bala.y - 50 &&
+                player.y + player.alto < bala.y);
+    }
+
+
 }
 
-// Cargar imágenes de destrucción
-const frames_destruccion = [];
-for (let i = 1; i <= 7; i++) {
-    const img = new Image();
-    img.src = `./imagenes/Enemigos/Animaciones/enemigo_destruccion_${i}.gif`;
-    frames_destruccion.push(img);
-}
 class Enemigo {
-    constructor(x, y, ancho, alto, puntos_hp, imagen_actual) {
+    constructor(x, y, ancho, alto, puntos_hp, imagen_actual, valor_puntos) {
         this.x = x;
         this.y = y;
         this.ancho = ancho;
         this.alto = alto;
         this.puntos_hp = puntos_hp;
         this.imagen_actual = imagen_actual;
-        this.destruido = false; 
-        this.frame_actual = 0; 
-        this.tiempo_frame = 100; 
-        this.last_update = Date.now(); // Para controlar el tiempo de actualización
-        this.animacion_terminada = false; // Para saber si la animación ha terminado
-        this.destruccion_timer = null
+        this.valor_puntos = valor_puntos;
+
     }
 
     dibujar() {
-        if (this.destruido) {
-            // Controlar el tiempo y mostrar la imagen correcta
-            const now = Date.now();
-            if (now - this.last_update > this.tiempo_frame) {
-                this.frame_actual++;
-                this.last_update = now;
-                if (this.frame_actual >= frames_destruccion.length) {
-                    this.animacion_terminada = true;
-                }
-            }
-            if (!this.animacion_terminada) {
-                ctx.drawImage(frames_destruccion[this.frame_actual], this.x, this.y, 50, 50);
-            }
-        } else {
-            ctx.drawImage(this.imagen_actual, this.x, this.y, 50, 50);
+        ctx.drawImage(enemigo_sprite, this.x, this.y, 50, 50);
+        if (this.puntos_hp <= 0 && !this.puntuacion_dibujada) {
+            this.dibujar_puntuacion();
         }
+
     }
 
     manejarColision(bala) {
@@ -146,40 +184,51 @@ class Enemigo {
             this.y + this.alto > bala.y;
     }
 
-    destruir() {
-        this.destruido = true;
-        this.destruccion_timer = setTimeout(() => {
 
-            //Elimina el enemigo después de mostrar el GIF durante 2 segundos
-            const index = enemigos_1.indexOf(this);
-            if (index > -1) {
-                enemigos_1.splice(index, 1);
-            }
-        }, 1000);
-    }
 
     disparar_enemigo() {
         let posicion_x = 0;
         let posicion_y = 0;
 
         //Dependiendo del lado del personaje, acomodar la posición de donde saldrá la bolita
-        posicion_x = 35;
-        posicion_y = -35;
-        balas.push({
+        posicion_x = 0;
+        posicion_y = 0;
+        balas_enemigo.push({
             x: this.x + posicion_x,
             y: this.y + posicion_y,
-            velocidad: velocidad_bala,
+            velocidad: velocidad_bala_enemigo,
             direccion: direccion
+
         });
-        sonido_enemigo_destruido();
+
     }
 
+    destruir_enemigo(enemigo) {
+        enemigos_1.splice(enemigo, 1);
+        puntuaciones.push({ x: this.x, y: this.y, texto: "+100", tiempo: 0 });
+        bandera_destruido = true;
+        sonido_enemigo_destruido();
+
+
+    }
+
+    dibujar_puntuacion() {
+        if (bandera_destruido) {
+            ctx.fillStyle = "black";
+            ctx.font = "40px Georgia";
+            ctx.fillText("+100", this.x, this.y, 40);
+            bandera_destruido = false
+
+
+        }
+
+    }
 
 }
 
 
 //Se crea el jugador
-const player = new Player(50, 750);
+const player = new Player(50, 750, 0, 0, 2);
 
 //AQUÍ, SE CREAN LAS FILAS DE LOS ENEMIGOS CON UN FOR QUE CREA LOS OBJETOS Y LOS ALMACENA EN UN ARREGLO
 let salto_posicion_x = 100;
@@ -198,7 +247,7 @@ while (bandera_filas < 3) {
     for (filas_enemigos; filas_enemigos < columnas_enemigos; filas_enemigos++) {
         console.log(filas_enemigos)
         enemigo1_x += salto_posicion_x;
-        enemigos_1[filas_enemigos] = new Enemigo(enemigo1_x, enemigo1_y, 50, 50, 1, imagen_enemigo);
+        enemigos_1[filas_enemigos] = new Enemigo(enemigo1_x, enemigo1_y, 50, 50, 1, imagen_enemigo, 100);
         console.log(enemigos_1[filas_enemigos])
     }
 
@@ -208,7 +257,6 @@ while (bandera_filas < 3) {
     columnas_enemigos += 10;
     console.log(columnas_enemigos)
     bandera_filas++;
-
 }
 /*
 TODO:
@@ -223,11 +271,11 @@ if (!pausa) {
 }
 setInterval(function () {
 
-    if (!pausa) {
+    if (!pausa && !mostrar_menu_inicio && !juego_terminado) {
         //Si los enemigos alcanzan el límite y deben bajar una vez
         if (alcanzado_limite && bajar_una_vez) {
             for (let i = enemigos_1.length - 1; i >= 0; i--) {
-                
+
                 enemigos_1[i].y += 50;  //Baja a todos los enemigos 50 puntos en Y
                 console.log("ada")
 
@@ -238,7 +286,7 @@ setInterval(function () {
                 } else {
                     enemigos_1[i].x -= 20;
                 }
-                
+
             }
 
             bajar_una_vez = false;
@@ -249,7 +297,7 @@ setInterval(function () {
             for (let i = enemigos_1.length - 1; i >= 0; i--) {
                 enemigos_1[i].x += 20;
                 //Verifica si algún enemigo alcanza el límite del canvas
-                if (enemigos_1[i].x + enemigos_1[i].ancho >= 1400) {
+                if (enemigos_1[i].x + enemigos_1[i].ancho >= canvas.width - 100) {
                     alcanzado_limite = true;
                     retroceder = true;
                     bajar_una_vez = true;
@@ -267,18 +315,74 @@ setInterval(function () {
                 }
             }
         }
-        enemigos_1[1].disparar_enemigo()
         //Vuelve a activar  el movimiento después de retroceder
 
     }
 }, 300);
 
+setInterval(function () {
+
+    if (!pausa && !mostrar_menu_inicio && !juego_terminado) {
+        let numero_aleatorio = Math.floor(Math.random() * enemigos_1.length);
+        enemigos_1[numero_aleatorio].disparar_enemigo()
+
+    }
+}, 1000);
+function volver_jugar() {
+    //Restablecer variables
+    direccion = "";
+    velocidad = 1.2;
+    obstaculos = [];
+    pausa = false;
+    score = 0;
+    puntuaciones.length = 0;
+    velocidad_bala = 2;
+    balas.length = 0;
+    balas_enemigo.length = 0;
+    velocidad_bala_enemigo = 1;
+    bandera_destruido = false;
+    mostrar_menu_inicio = true;
+    juego_terminado = false;
+
+    //Restablecer el estado del jugador
+    player.x = 50;
+    player.y = 750;
+    player.puntos_hp = 2;
+
+    //Reconfigurar los enemigos
+    enemigos_1.length = 0;
+    let salto_posicion_x = 100;
+    let salto_posicion_y = 70;
+    let enemigo1_x = 300;
+    let enemigo1_y = 100;
+    let filas_enemigos = 0;
+    let columnas_enemigos = 10;
+    let bandera_filas = 0;
+
+    while (bandera_filas < 3) {
+        for (filas_enemigos; filas_enemigos < columnas_enemigos; filas_enemigos++) {
+            enemigo1_x += salto_posicion_x;
+            enemigos_1[filas_enemigos] = new Enemigo(enemigo1_x, enemigo1_y, 50, 50, 1, imagen_enemigo, 100);
+        }
+
+        enemigo1_x = 300;
+        enemigo1_y += salto_posicion_y;
+        columnas_enemigos += 10;
+        bandera_filas++;
+    }
+
+    // Reiniciar fondo y música
+    fondo_juego.onload = function () {
+        ctx.drawImage(fondo_juego, 0, 0, canvas.width, canvas.height);
+    };
+   
+}
 paint();
 var teclapresionada = {};
 document.addEventListener("keydown", function (e) {
     teclapresionada[e.code] = true;
 
-    if (!pausa) {
+    if (!pausa && !mostrar_menu_inicio && !juego_terminado) {
         if (teclapresionada['KeyA']) {
             direccion = "izquierda";
             player.x -= velocidad;
@@ -286,10 +390,25 @@ document.addEventListener("keydown", function (e) {
         if (teclapresionada['KeyD']) {
             direccion = "derecha";
             player.x += velocidad;
+
         }
+
 
         if (teclapresionada['KeyC']) {
             player.disparar();
+        }
+    }
+    if (teclapresionada['Enter']) {
+        if (juego_terminado) {
+            musica_fondo.pause();
+            musica_fondo.currentTime = 0;
+            volver_jugar();
+        } else {
+            mostrar_menu_inicio = false;
+            setTimeout(() => {
+                musica_fondo.play();
+                musica_fondo.loop = true;
+            }, 1000);
         }
     }
 
@@ -305,10 +424,10 @@ document.addEventListener("keyup", function (e) {
 
 
 function update() {
-    if (!pausa) {
+    if (!pausa && !mostrar_menu_inicio && !juego_terminado) {
         switch (direccion) {
             case "izquierda":
-                if (velocidad == 0 && player.x >= 1400) {
+                if (velocidad == 0 && player.x >= canvas.width - 120) {
                     velocidad = 1.2;
                     console.log(velocidad)
                 }
@@ -324,7 +443,7 @@ function update() {
                     console.log(velocidad);
                 }
                 player.x += velocidad;
-                if (player.x >= 1400) {
+                if (player.x >= canvas.width - 120) {
                     velocidad = 0;
                     console.log(player.x)
 
@@ -351,10 +470,9 @@ function update() {
                     //Si el enemigo tiene 0 puntos de vida, eliminarlo
                     if (enemigo.puntos_hp <= 0) {
                         console.log('Enemigo eliminado');
-                        // Muestra el GIF durante 2 segundos antes de eliminar el enemigo
-
-                        enemigo.destruir();
-                        sonido_enemigo_destruido();
+                        score += enemigo.valor_puntos;
+                        enemigos_1[i].destruir_enemigo(i);
+                        bandera_destruido = true;
 
 
 
@@ -367,10 +485,23 @@ function update() {
         //ACTUALIZACIÓN DE BALA PARA LOS ENEMIGOS
         balas_enemigo.forEach((bala, balaIndex) => {
             bala.y += bala.velocidad;
-            ctx.drawImage(imagen_bala, bala.x, bala.y, 30, 30);
+            ctx.drawImage(imagen_bala, bala.x, bala.y, 50, 50);
 
-            //Revisa colisiones entre las balas y los enemigos
-            
+            if (player.manejarColision(bala)) {
+                console.log('Colisión detectada con jugador');
+
+                //Eliminar la bala
+                balas_enemigo.splice(balaIndex, 1);
+                player.puntos_hp--;
+                console.log(player.puntos_hp);
+                sonido_player_dano();
+
+                //Si el jugador tiene 0 puntos de vida, eliminarlo
+                if (player.puntos_hp <= 0) {
+                    console.log('Jugador eliminado');
+                    juego_terminado = true;
+                }
+            }
         });
 
 
@@ -385,24 +516,46 @@ function paint() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     //fondo del canvas
-    
+
+
     ctx.drawImage(fondo_juego, 0, 0, canvas.width, canvas.height);
 
     player.dibujar();
     for (let i = enemigos_1.length - 1; i >= 0; i--) {
         enemigos_1[i].dibujar();
+
     }
+    // Dibuja las puntuaciones
+    ctx.fillStyle = "black";
+    ctx.font = "40px Georgia";
+    for (let i = puntuaciones.length - 1; i >= 0; i--) {
+        let puntuacion = puntuaciones[i];
+        ctx.fillText(puntuacion.texto, puntuacion.x, puntuacion.y - puntuacion.tiempo);
+        puntuacion.tiempo += .4; // Cambia la velocidad de desaparición aquí
+        if (puntuacion.tiempo > 50) { // Después de 50 píxeles de movimiento, elimina la puntuación
+            puntuaciones.splice(i, 1);
+        }
+    }
+
     ctx.font = "30px Georgia";
     ctx.fillStyle = "gray";
     ctx.fillText("Score: " + score, 100, 30);
 
     if (pausa) {
         ctx.fillStyle = "rgba(100,100,100,.5)"
-        ctx.fillRect(0, 0, canvas.height, canvas.width)
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
         ctx.fillStyle = "black";
         ctx.font = "100px Georgia";
         ctx.fillText("Pausa", (canvas.width / 2) - 100, canvas.height / 2);
     }
+    if (mostrar_menu_inicio) {
+        menu_inicio();
+
+    }
+    if (juego_terminado) {
+        game_over();
+    }
+
     update();
     requestAnimationFrame(paint)
 
@@ -419,6 +572,9 @@ window.requestAnimationFrame = (function () {
             window.setTimeout(callback, 17);
         };
 }());
+
+
+
 
 
 
